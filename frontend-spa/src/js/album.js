5375d69cddb0a0314f4909e62cefd8a0bee49cd6
TaylourH <harristaylour@gmail.com>
2022-07-26 09:54:38 -0400

    cleaned the frontend folder, updated css, updated images in populator

--- frontend-spa/src/js/album.js
+++ frontend-spa/src/js/album.js
@@ -1,64 +1,139 @@
-export default function album(album) {
+
+
+export default function albumView(album) {
     return `
-    <div class="fix">
-    <div class="mainContainer">
-    <div class="top">
-        <div class="albumGroup">
-            <div class="albumPart1">
-                <h4 class="artist">${album.artist}</h4>
-                <h4 class="recordLabel">${album.recordLabel}</h4>
-                <div class="albumRating">
-                    <div class="ratingCombo">
-                        <h4>Album Rating</h4>
-                        <div class="rating">
-                            <p class="starRating">${album.avgRating}</p>
-                            <p class="fiveStars">/5</p>
+    <section class="sodaMachineMiddle">
+    <section class="albumGroup">
+        <div class="albumGroupPart1">
+            <div class="albumHeader">
+                <h2 class="albumName">${album.title} <span class="artistName">${album.artist}</span></h2>
+            </div>
+        </div>
+        <div class="albumGroupPart2">
+            <div class="albumBox">
+                <div class="boxBottle">
+                    <img src="/images/sodaSoundLogo bottle only cropped.png" alt="">
+                </div>
+                <div class="albumCard">
+                    <div class="test">
+                        <img src="${album.imageUrl}" class="albumPic" alt="">
+                    </div>
+                    <div class="cardText">
+                        <ul>
+                            <li>${album.title}</li>
+                            <li>${album.artist}</li>
+                            <li>${album.recordLabel}</li>
+                            <li>${album.duration}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="albumRatingReviewCombo">
+                    <div class="albumRating">
+                        <h5>Album Rating</h5>
+                        <div class="combinedRating">
+                            <p class="rating1">${album.avgRating}</p>
+                            <p class="rating2">/</p>
+                            <p class="rating3">5</p>
+                        </div>
+                    </div>
+                    <div class="albumReviews">
+                        <a  href="#popup3">
+                            <h4 class="reviewPop">Reviews</h4>
+                        </a>
+                    </div>
+                </div>
+                <div id="popup3" class="overlay">
+                    <div class="popup">
+                        <div class="albumReviewsHeading">
+                            <h4>Album Reviews</h4>
+                        </div>
+                        <a class="close" href="#">&times;</a>
+                        <div class="content"> 
+                        ${album.comments.map(comment => {
+        return `                           
+                             <ul>
+                            <li>
+                                <p>${comment.content}
+                                </p>
+                                <h5>${comment.author}</h5>
+                            </li>
+                        </ul>
+                        `
+    }).join("")
+        }
+                            <div class="albumLeaveReview">
+                                <input type="text" placeholder="reviewer's name">
+                                <input type="text" placeholder="write a review...">
+                                <button type="submit">Submit</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="albumPart2">
-                <h2 class="albumSelected">${album.title}</h2>
-                <img src=${album.imageUrl} class="albumPicMiddle" alt="">
+        </div>
+    </section>
+    <section class="trackListings"> 
+    ${album.songs.map(song => {
+            return `
+         <ol>
+        <div class="trackButton">
+            <a class="aboutUs" href="${'#' + song.title}">
+                <li>${song.title}</li>
+            </a>
+        </div>
+    </ol>
+    `
+}).join("")
+}
+${album.songs.map(song => {
+    return `
+<div id="${song.title}" class="overlay">
+        <div class="popup">
+            <div class="songInfoGroup">
+                <h3>${song.title}</h3>
+                <ul>
+                    <li>${song.duration}</li>
+                    <li class="songRating">
+                        <h5>Rating</h5>
+                        <div>
+                            <p class="rating1">${song.rating}</p>
+                            <p class="rating2">/</p>
+                            <p class="rating3">5</p>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div class="albumPart3">
-                <div class="albumReviews">
-                    <h4>Album Reviews</h4>
-                    ${album.comments.map(comment => {
-        return `
-                            <div class="review1">
-                            <h4>${comment.author}</h4>
-                            <p>${comment.content}</p>
-
-                            </div>
-                        `
-    }).join("")
-        }  
-                </div>
-                <div class="albumLeaveReview">
-                    <input type="text" placeholder="write a review...">
+            <a class="close" href="#">&times;</a>
+            <div class="content">
+                <div class="songLeaveRating">
+                    <label for="stars">Rate (0-5):</label>
+                    <input type="number" name="stars" min="0" max="5">
                     <button type="submit">Submit</button>
                 </div>
             </div>
+            ${song.comments.map(comment => {
+                return `                           
+                 <ul>
+                <li>
+                    <p>${comment.content}
+                    </p>
+                    <h5>${comment.author}</h5>
+                </li>
+            </ul>
+            `
+            }).join("")
+                }  
+            <div class="songLeaveReview">
+                <input type="text" placeholder="reviewer's name">
+                <input type="text" placeholder="write a review...">
+                <button type="submit">Submit</button>
+            </div>
         </div>
     </div>
-    <div class="middle">
-        <div class="trackListings">
-            <h3>Album Tracks</h3>
-            ${album.songs.map(song => {
-            return `
-                        <ol>
-                        <div class="trackButton">
-
-                                <li>${song.title}</li>
-
-                        </div>
-                        </ol>
-                        `
+</section>
+    `
         }).join("")
-        }  
-        </div>
-    </div>
-</div>
+        }
+
     `
 }
--- frontend-spa/src/js/song.js
+++ frontend-spa/src/js/song.js
@@ -1,49 +1,5 @@
 export default function makeSong (song) {
     return `
 
-    <div id=${song.title} class="overlay">
-        <div class="popup">
-            <div class="songInfoGroup">
-                <h3>${song.title}</h3>
-                <ul>
-                    <li>${song.duration}</li>
-                    <li class="songRating">
-                        <h5>Rating</h5>
-                        <div>
-                            <p class="rating1">${song.rating}</p>
-                            <p class="rating2">/</p>
-                            <p class="rating3">5</p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <a class="close" href="#">&times;</a>
-            <div class="content">
-                <div class="songLeaveRating">
-                    <label for="stars">Rate (0-5):</label>
-                    <input type="number" name="stars" min="0" max="5">
-                    <button type="submit">Submit</button>
-                </div>
-            </div>
-            ${song.comments.map(comment => {
-                return`                           
-                 <ul>
-                <li>
-                    <p>${comment.content}
-                    </p>
-                    <h5>${comment.author}</h5>
-                </li>
-            </ul>
-            `
-        }).join("")
-            }  
-            <div class="songLeaveReview">
-                <input type="text" placeholder="reviewer's name">
-                <input type="text" placeholder="write a review...">
-                <button type="submit">Submit</button>
-            </div>
-        </div>
-    </div>
-</section>
     `
 }
