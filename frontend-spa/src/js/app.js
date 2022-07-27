0af5a9bb87f3d5884ea614887fd43a5c2aff2018
TaylourH <harristaylour@gmail.com>
2022-07-27 00:09:32 -0400

    updated css, added animations, added trashcan

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -6,25 +6,25 @@ import homeView from "./home.js";
 const container = document.querySelector(".anchor");
 
 
-function makeHomeView(){
+function makeHomeView() {
     fetch(`http://localhost:8080/api/albums`)
-    .then(res => res.json())
-    .then(albums =>{
-        console.log(albums);
-        container.innerHTML = header();
-        container.innerHTML += homeView(albums);
-        container.innerHTML += footer();
-        
-        const albumViewBtn = document.querySelectorAll(".albumCard")
-        albumViewBtn.forEach(albumEl =>{
-            const albumIdEl = albumEl.querySelector(".album-id");
-            albumEl.addEventListener("click", () =>{
-                makeAlbumView(albumIdEl.value);
+        .then(res => res.json())
+        .then(albums => {
+            console.log(albums);
+            container.innerHTML = header();
+            container.innerHTML += homeView(albums);
+            container.innerHTML += footer();
+
+            const albumViewBtn = document.querySelectorAll(".albumCard")
+            albumViewBtn.forEach(albumEl => {
+                const albumIdEl = albumEl.querySelector(".album-id");
+                albumEl.addEventListener("click", () => {
+                    makeAlbumView(albumIdEl.value);
+                })
             })
         })
-    })
 
-    
+
 }
 
 function makeAlbumView(albumId) {
@@ -38,48 +38,48 @@ function makeAlbumView(albumId) {
             container.innerHTML += footer();
 
             const goHomeBtn = document.querySelector(".siteNameLogo");
-            goHomeBtn.addEventListener("click", ()=> {
+            goHomeBtn.addEventListener("click", () => {
                 makeHomeView();
             })
             const commentAuthorIN = document.querySelector(".reviewName");
             const commentContentIN = document.querySelector(".reviewContent");
             const commentBtn = document.querySelector(".reviewSubmit");
 
-            commentBtn.addEventListener("click", ()=> {
-            const newCommentJson = {
-                "author" : commentAuthorIN.value, 
-                "content" : commentContentIN.value,
-            }    
-            fetch(`http://localhost:8080/api/album/${albumNumber.id}/addComment`,{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(newCommentJson)
-        
+            commentBtn.addEventListener("click", () => {
+                const newCommentJson = {
+                    "author": commentAuthorIN.value,
+                    "content": commentContentIN.value,
+                }
+                fetch(`http://localhost:8080/api/album/${albumNumber.id}/addComment`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(newCommentJson)
+
+                })
+                    .then(res => res.json())
+                    .then(albumNumber => {
+                        makeAlbumView(albumNumber.id);
+                    })
+
             })
-            .then(res => res.json())
-            .then(albumNumber => {
-                makeAlbumView(albumNumber.id);
-            } )
-            
-            })
-            const deleteBtn= document.querySelector(".delete-button");
+            const deleteBtn = document.querySelector(".delete-button");
             const albumIdEl = document.querySelector(".album-id");
-    deleteBtn.addEventListener("click", () =>{
-    fetch(`http://localhost:8080/api/album/${albumIdEl.value}`,{
-      method:'DELETE'  
-    })
-    .then(res => res.json())
-    .then(newAlbums =>{
-        makeHomeView(newAlbums);
-    })
-    .catch(err => console.error(err))
-})
+            deleteBtn.addEventListener("click", () => {
+                fetch(`http://localhost:8080/api/album/${albumIdEl.value}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(newAlbums => {
+                        makeHomeView(newAlbums);
+                    })
+                    .catch(err => console.error(err))
+            })
         })
         .catch(err => console.error(err))
-        
-        
+
+
 }
 
 makeHomeView();
